5ace66c5751520f645601ad6592d288a6ce9c7bf
Дмитрий Андреев <djandreev89@mail.ru>
2019-09-08 15:24:31 +1000

    Добавьте файлы проекта.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер таблицы по ширине: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите размер таблицы по высоте: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            int count = 1;
+            int paint = 1;
+            int paint2 = width;
+            Random rnd = new Random();
+
+            for (int i = 1; i <= width * height; i++)
+            {
+                Console.ResetColor();
+                int number = rnd.Next(1, width * height);
+
+                if (i == paint && i <= width * width)
+                {
+                    paint += width + 1;
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                }
+
+                if (i == paint2 && i <= width * width)
+                {
+                    paint2 += width - 1;
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                }
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+
+                if (number % 2 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                Console.Write($"{number,4}");
+
+                if (i % width * count == 0)
+                {
+                    count++;
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
